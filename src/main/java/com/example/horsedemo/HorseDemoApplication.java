ac6f1ba329ec57171adb3eda9287c74b661a3184
JustShooter <milicia@tut.by>
2022-06-26 16:58:55 +0300

    Final version. Liquibase working. Added JS form validation on new horse. Added HTML5 form validation on edit horse.

---
+++ src/main/java/com/example/horsedemo/HorseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.horsedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HorseDemoApplication.class, args);
+    }
+
+}
